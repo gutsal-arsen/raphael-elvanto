2747d672f8ad882ef00fe4df690a79ea366e639d
Vitaly Pestov <ariekdev@gmail.com>
2016-04-02 21:29:24 +0300

    calculate gMap center position and peoples markers for any search

--- routes/search.js
+++ routes/search.js
@@ -8,7 +8,8 @@ var table_fields = {
   'lastname': true,
   'gender': true,
   'home_city': true,
-  'home_address': true
+  'home_address': true,
+  'loc': true
 };
 
 var findPeoplesByCity = function(city, callback) {
@@ -33,6 +34,33 @@ var findPeoplesByLoc = function(lng, lat, rad, callback) {
   });
 };
 
+var calculateGMapPosition = function(peoples) {
+  // google map center position - first matched user from array, if any :
+  if (peoples.length > 0) {
+    var location = peoples[0].loc;
+    if (location.length > 0) {
+      var gMapPos = [location[0], location[1]]; // lng, lat
+      return gMapPos;
+    }
+  }
+  return [0,0]
+};
+
+// Produce peoples markers string in form "lng1,lat1,name1;lng2,lat2,name2" for further easy parsing
+var getPeoplesMarkers = function(peoples) {
+  var peoplesLength = peoples.length;
+  var markers = "";
+  for (var i = 0; i < peoplesLength; i++) {
+    if (i > 0) markers += ';';
+    var lng = peoples[i].loc[0],
+        lat = peoples[i].loc[1],
+    // quickFix: replace our 'special' symbols if they occurs in concatenated string
+        markerName = (peoples[i].firstname + ' ' + peoples[i].lastname + ' at ' + peoples[i].home_address).split(',').join(' ').split(';').join(' ');
+    markers += (lng + ',' + lat + ',' + markerName);
+  }
+  return markers;
+};
+
 /* GET users listing. */
 router.get('/', function (req, res) {
   res.render('search', {title: 'Search'});
@@ -49,12 +77,16 @@ router.post('/', function (req, res) {
   switch (search_type) {
     case 'city':
       findPeoplesByCity(search_term, function (err, results) {
-        res.render("search_results", {peoples: results});
+        var gMapPos = calculateGMapPosition(results);
+        var markers = getPeoplesMarkers(results);
+        res.render("search_results", {peoples: results, gMapPos: gMapPos, markers: markers});
       });
       break;
     case 'street':
       findPeoplesByStreetAddress(search_term, function (err, results) {
-        res.render("search_results", {peoples: results});
+        var gMapPos = calculateGMapPosition(results);
+        var markers = getPeoplesMarkers(results);
+        res.render("search_results", {peoples: results, gMapPos: gMapPos, markers: markers});
       });
       break;
     case 'loc':
@@ -67,7 +99,8 @@ router.post('/', function (req, res) {
         lat = parseFloat(search_term_params[1]),
         rad = parseFloat(search_term_params[2]);
       findPeoplesByLoc(lng, lat, rad, function (err, results) {
-        res.render("search_results", {peoples: results});
+        var markers = getPeoplesMarkers(results);
+        res.render("search_results", {peoples: results, gMapPos: [lng, lat], markers: markers});
       });
       break;
     default:
