342c69cff14ba575118f660d31e31ffc7a2cf9fc
Vitaly Pestov <ariekdev@gmail.com>
2016-04-15 20:34:12 +0300

    create google contact export route

--- routes/crawl.js
+++ routes/crawl.js
@@ -180,4 +180,117 @@ router.get('/', function (req, res) {
   });
 });
 
+router.post('/elvanto_to_google', function (req, res) {
+  var attrHash = req.body;
+  var oauthCode = attrHash['oauth_code'],
+      oauthTokens = attrHash['oauth_tokens'];
+
+  var accessToken = oauthTokens.access_token;
+  // TODO: example to real contacts
+  getExampleElvantoContacts(function (err, peoples) {
+    if (err) {
+      return res.send("An error getElvantoContacts: " + err );
+    }
+    createGoogleContactGroup('elvanto', oauthTokens, function (err, groupName) {
+      if (err) {
+        return res.send("An error createGoogleContactGroup: " + err );
+      }
+      // TODO: rewrite loop
+      for (var person in peoples) {
+        createGoogleContact(person, groupName, accessToken);
+      }
+      return res.send("Ok");
+    })
+  });
+});
+
+var getExampleElvantoContacts = function(callback) {
+  var table_fields = {
+    'firstname': true,
+    'lastname': true,
+    'email': true,
+    'phone': true,
+    'home_city': true,
+    'home_address': true
+  };
+  People.find({id: "af472139-e8bb-11e4-af42-0673d9c9b5d6"}, table_fields, function(err, peoples) {
+    callback(err, peoples);
+  });
+};
+
+var getElvantoContacts = function (callback) {
+  var table_fields = {
+    'firstname': true,
+    'lastname': true,
+    'email': true,
+    'phone': true,
+    'home_city': true,
+    'home_address': true
+  };
+  People.find({}, table_fields, function(err, peoples) {
+    callback(err, peoples);
+  });
+}
+
+var createGoogleContactGroup = function(groupName, accessToken, callback) {
+  // TODO: please, make a group <groupName> manually for now!
+  callback(null, groupName);
+}
+
+var createGoogleContact = function(contact, groupName, accessToken, callback) {
+  // TODO: groupName is not used yet
+  /* TODO: Contact as a proxy model for Google contact entity:
+   Contact.storeOne(person, function (err, body) {   }); */
+  var atomXml =
+    "<atom:entry xmlns:atom='http://www.w3.org/2005/Atom' xmlns:gd='http://schemas.google.com/g/2005'>" +
+    "<atom:category scheme='http://schemas.google.com/g/2005#kind' term='http://schemas.google.com/contact/2008#contact'/>" +
+    "<gd:name> " +
+    "  <gd:givenName>_GIVENNAME_</gd:givenName> " +
+    "  <gd:familyName>_FAMILYNAME_</gd:familyName> " +
+    "  <gd:fullName>_FULLNAME_</gd:fullName> " +
+    "</gd:name> " +
+    "<atom:content type='text'>Notes</atom:content> " +
+    "<gd:email rel='http://schemas.google.com/g/2005#work' primary='true' address='_WORKEMAIL_'" +
+    "<gd:email rel='http://schemas.google.com/g/2005#home' address='_HOMEEMAIL_'/> " +
+    "<gd:phoneNumber rel='http://schemas.google.com/g/2005#work' primary='true'>_WORKPHONENUMBER_</gd:phoneNumber> " +
+    "<gd:phoneNumber rel='http://schemas.google.com/g/2005#home'>_HOMEPHONENUMBER_</gd:phoneNumber> " +
+    "<gd:structuredPostalAddress rel='http://schemas.google.com/g/2005#work' primary='true'> " +
+    "<gd:city>_HOMECITY_</gd:city> " +
+    "<gd:street>_HOMESTREET_</gd:street> " +
+    "<gd:region>_HOMEREGION_</gd:region> " +
+    "<gd:postcode>_HOMEPOSTALCODE_</gd:postcode> " +
+    "<gd:country>_HOMECOUNTRY_</gd:country> " +
+    "<gd:formattedAddress>_HOMEFORMATTEDADDRESS_</gd:formattedAddress> " +
+    "</gd:structuredPostalAddress> " +
+    "</atom:entry> ";
+
+  var replacedAtomXml = atomXml
+    .replace(/_GIVENNAME_/g, contact.firstname)
+    .replace(/_FAMILYNAME_/g, contact.lastname)
+    .replace(/_FULLNAME_/g, contact.firstname + ' ' + contact.lastname)
+    .replace(/_WORKEMAIL_/g, contact.email)
+    .replace(/_HOMEEMAIL_/g, contact.email)
+    .replace(/_WORKPHONENUMBER_/g, contact.phone)
+    .replace(/_HOMEPHONENUMBER_/g, contact.phone)
+    .replace(/_HOMECITY_/g, contact.home_city)
+    .replace(/_HOMESTREET_/g, contact.home_address)
+    .replace(/_HOMEREGION_/g, '')
+    .replace(/_HOMEPOSTALCODE_/g, contact.home_postcode)
+    .replace(/_HOMECOUNTRY_/g, contact.home_country)
+    .replace(/_HOMEFORMATTEDADDRESS_/g, contact.home_address);
+
+  request({
+    method: 'POST',
+    uri: 'https://www.google.com/m8/feeds/contacts/default/full/',
+    contentType: 'application/atom+xml',
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    },
+    json: true,
+    body: replacedAtomXml
+  }, function (error, response, body) {
+    return;
+  });
+}
+
 module.exports = router;
