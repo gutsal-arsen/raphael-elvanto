f782d84bd08ec03c6dbb0fd75c03998a0650aec3
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-04-28 12:07:18 +0300

    added DOM manipulation hander. If .icon-bell.badge content changed, we will either show or hide it

---
+++ public/js/ui.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    /**
+     if .icon-bell.badge content changed, we will either show or hide it
+     */
+    $(".icon-bell .badge").bind('DOMSubtreeModified',(e) => {
+	if(parseInt($(e.target).text()) > 0){
+	    $(e.target).removeClass('none');
+	} else {
+	    $(e.target).addClass('none');
+	}
+    });
+});
