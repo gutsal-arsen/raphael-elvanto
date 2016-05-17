e3eeed96fe8670c79124bf338f05d5186e78cda3
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-05-17 09:16:01 +0300

    Widget, WidgetContainer, ProgressWidget classes added

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,20 +1,62 @@
+class Widget {
+  // Construct container
+  // @param [pTempl] selector string pointing to mustache template script element
+  // @param [pSel] parent selector
+  // @return constructed element
+  constructor(templSel, parentSel, options) {
+    this.templEl = $(templSel);
+    this.parentEl = $(parentSel);
+
+    this.body = Mustache.render($(this.templEl).html(), options);
+    this.el = $(this.body);
+
+    $(this.parentEl).append(this.el);
+
+  }
+}
+class WidgetContainer extends Widget {
+
+  constructor(templSel, parentSel, options){
+    super(templSel, parentSel, options);
+
+  }
+
+  add(widget){
+    this.widgets = this.widgets || [];
+
+     this.widgets.push(widget);
+    $('.icon-bell .badge').text(new String(this.widgets.length))
+    if(this.widgets.length){
+      $(".icon-bell .badge").removeClass('none');
+    } else {
+	    $(".icon-bell .badge").addClass('none');
+    }
+  }
+}
+class ProgressWidget extends Widget{
+  // Construct container
+  // @param [pTempl] selector string pointing to mustache template script element
+  // @param [pSel] parent selector
+  // @return constructed element
+  constructor(templSel, parentSel, options) {
+    super(templSel, parentSel, options)
+    this.total = options.total;
+  }
+
+  // Update widget state
+  // @param [options] options
+  update(o){
+    o.progress = o.progress || this.total?(o.current / this.total) * 100:0;
+    o.text && $('span.clear.block.m-b-none > span', this.el).text(o.text);
+    o.progress && $('.progress-bar', this.el).css('width', o.progress).data('original-title', o.title);
+  }
+
+  remove(){
+    $(this.el).remove();
+  }
+}
+
 $(document).ready(() => {
 
   $('#side-menu').metisMenu();
-  /**
-     if .icon-bell.badge content changed, we will either show or hide it
-  */
-
-  $('.dropdown .dropdown-menu .panel .list-group').bind('DOMSubtreeModified', (e) => {
-    $(".icon-bell .badge").text($('a', e.target).size());
-  })
-  $(".icon-bell .badge").bind('DOMSubtreeModified',(e) => {
-	  if(parseInt($(e.target).text()) > 0){
-	    $(e.target).removeClass('none');
-	  } else {
-	    $(e.target).addClass('none');
-	  }
-  });
-  $(".icon-bell .badge").text($($('.dropdown .dropdown-menu .panel .list-group a')).size());
-
 });
